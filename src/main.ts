520a28076870667f71a7d0af37778c35474cc24a
Jordan <jordangopie@live.com>
2023-07-30 22:08:17 -0400

    Remove Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  const config = new DocumentBuilder()
-    .setTitle('Bluelight Time Management')
-    .setDescription('The API For Bluelight Time Management')
-    .setVersion('0.1')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
   await app.listen(port);
 }
 bootstrap();
