017e4ea2a3d1217f69a8d08fe46e451f530caa35
Jordan <jordangopie@live.com>
2023-08-03 09:52:36 -0400

    Add Procfile

--- prisma/migrations/20230801023140_init/migration.sql
+++ prisma/migrations/20230801023140_init/migration.sql
@@ -28,4 +28,3 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
